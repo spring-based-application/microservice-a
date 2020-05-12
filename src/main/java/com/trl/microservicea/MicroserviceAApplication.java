caebebdbe7512cfb4f3425c5aff28c95adc45afe
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-05-12 17:44:34 +0200

    Configuration Eureka Client.

--- src/main/java/com/trl/microservicea/MicroserviceAApplication.java
+++ src/main/java/com/trl/microservicea/MicroserviceAApplication.java
@@ -2,12 +2,14 @@ package com.trl.microservicea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceAApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceAApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAApplication.class, args);
+    }
 
 }
