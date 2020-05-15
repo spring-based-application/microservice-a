aad5f986f0f2c65855d067a2dcccca40acf6ca52
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-05-15 13:27:03 +0200

    Refactoring. Adding support Cycle Breaker Hystrix.

---
+++ src/main/java/com/trl/microservicea/app/AppService.java
@@ -0,0 +1,8 @@
+package com.trl.microservicea.app;
+
+public interface AppService {
+
+    String checkConnectionBetweenMicroservices ();
+
+    String getInfo();
+}
---
+++ src/main/java/com/trl/microservicea/app/AppServiceImpl.java
@@ -0,0 +1,56 @@
+package com.trl.microservicea.app;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class AppServiceImpl implements AppService {
+
+    private final EurekaClient eurekaClient;
+    private final RestTemplate restTemplate;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    public AppServiceImpl(@Lazy @Qualifier("eurekaClient") EurekaClient eurekaClient, RestTemplate restTemplate) {
+        this.eurekaClient = eurekaClient;
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public String checkConnectionBetweenMicroservices() {
+        StringBuilder result = new StringBuilder();
+
+        String fromMicroserviceA = getInfo();
+        String fromMicroserviceB = restTemplate.getForObject("http://MICROSERVICE-B/app/getInfo", String.class);
+        String fromMicroserviceC = restTemplate.getForObject("http://MICROSERVICE-C/app/getInfo", String.class);
+        String fromMicroserviceD = restTemplate.getForObject("http://MICROSERVICE-D/app/getInfo", String.class);
+        String fromMicroserviceE = restTemplate.getForObject("http://MICROSERVICE-E/app/getInfo", String.class);
+
+
+        result.append(fromMicroserviceA);
+        result.append("\n");
+        result.append(fromMicroserviceB);
+        result.append("\n");
+        result.append(fromMicroserviceC);
+        result.append("\n");
+        result.append(fromMicroserviceD);
+        result.append("\n");
+        result.append(fromMicroserviceE);
+
+        return result.toString();
+    }
+
+    @Override
+    public String getInfo() {
+        InstanceInfo instanceInfo = eurekaClient.getApplication(appName).getInstances().get(0);
+
+        return String.format("(microservice-a)(GetInfo)(Application name: '%s')(Host: '%s')(Port: '%s')",
+                instanceInfo.getAppName(), instanceInfo.getHostName(), instanceInfo.getPort());
+    }
+}
