b13dd18674df6dd314751c015954af2c2103f9c1
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-05-17 17:32:51 +0200

    Configuration Hystrix.

--- src/main/java/com/trl/microservicea/appInfo/AppInfoServiceImpl.java
+++ src/main/java/com/trl/microservicea/appInfo/AppInfoServiceImpl.java
@@ -3,6 +3,7 @@ package com.trl.microservicea.appInfo;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
@@ -31,7 +32,23 @@ public class AppInfoServiceImpl implements AppInfoService{
                 instanceInfo.getAppName(), instanceInfo.getHostName(), instanceInfo.getPort());
     }
 
-    @HystrixCommand(fallbackMethod = "getFallbackInfoByExternalMicroservice")
+    @HystrixCommand(fallbackMethod = "getFallbackInfoByExternalMicroservice",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "1000"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10"),
+                    @HystrixProperty(name = "execution.isolation.strategy", value = "THREAD"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "20000"),
+                    @HystrixProperty(name = "metrics.rollingPercentile.timeInMilliseconds", value = "20000"),
+                    @HystrixProperty(name = "metrics.healthSnapshot.intervalInMilliseconds", value = "5000"),
+                    @HystrixProperty(name = "fallback.isolation.semaphore.maxConcurrentRequests", value = "100")
+            },
+            threadPoolKey = "microserviceInfoPool",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "20"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10"),
+            })
     @Override
     public String getInfoByExternalMicroservice(String url) {
         return restTemplate.getForObject(url, String.class);
